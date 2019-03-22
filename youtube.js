cf4bda904ab92a4bb942a27eeab22b39b6b2fbfb
Victoria Strenadko <31935971+vikstr@users.noreply.github.com>
2019-03-22 23:00:26 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,23 +1,32 @@
-var url = 'https://www.youtube.com/watch?v=K7CK-Fx4z-c';
+var url = window.location.href;
+/*var getActiveUrl = document.addEventListener('DOMContentLoaded', function() {
+	alert("URL");
+    var checkPageButton = document.getElementById('life');
+    checkPageButton.addEventListener('click', function() { 
+        chrome.tabs.query({active:true},function(tabsArray) {
+			var tab = tabsArray[0];
+			var tabUrl = tab.url;
+			url = tabUrl; 
+			alert(url);
+        });
+    },false);
+},false);*/
+function getCurrentTime(){
+	var elemCurrentTime = document.getElementsByClassName("ytp-time-current");
+	return elemCurrentTime[0].textContent;
+}
+function getDurationTime(){
+	var elemDurationTime = document.getElementsByClassName("ytp-time-duration");
+	return elemDurationTime[0].textContent;
+}
 var elemButtonPlay = document.getElementsByClassName('ytp-play-button');
 elemButtonPlay[0].addEventListener("click", function(){
 	const xhr = new XMLHttpRequest();
 	xhr.open('GET',url);
 	xhr.send();
-	console.log("send")
+	alert("send");
+	var currentTime = getCurrentTime();
+	alert(currentTime);
+	var durationTime = getDurationTime();
+	alert(durationTime);
 });
-
-function getCurrentTime(){
-	var elemCurrentTime = document.getElementsByClassName("ytp-time-current");
-	return elemCurrentTime[0].textContent;
-}
-currentTime = getCurrentTime();
-console.log(currentTime);
-function getDurationTime(){
-	var elemDurationTime = document.getElementsByClassName("ytp-time-duration");
-	return elemDurationTime[0].textContent;
-}
-var durationTime = getDurationTime();
-console.log(durationTime);
-
-
