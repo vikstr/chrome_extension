6b7c9c9842825964d0423d0a57bea6becd807a7b
Victoria <31935971+vikstr@users.noreply.github.com>
2019-04-26 20:16:19 +0300

    Add files via upload

--- message.js
+++ message.js
@@ -1,37 +1,97 @@
-var bod = document.getElementsByTagName('body')[0];
-bod.insertAdjacentHTML('afterbegin', `
-    <div style = "position:fixed;
-				  display:inline;
-				  width:480px;
-				  height:104px;
-				  top:45%;
-				  left:35%;
-				  right:50%;
-				  background-color:rgba(14, 14, 14, 0.4);
-				  border:0.75px solid #FFFFFF;
-				  border-radius:30px;
-				  z-index:1;
-				  cursor:pointer;">
-		<div style="position:absolute;
-			top:30%;
-			left:30%;
-			font-family: Roboto;
-			font-style: normal;
-			font-weight: normal;
-			font-size: 24px;
-			line-height: normal;
-			color:white;
-			transform:translate(-50%,-50%);
-			-ms-transform:translate(-50%,-50%);">Введите сообщение...
+var elemFullScreen = document.getElementsByClassName('ytp-fullscreen-button');
+var flag = 1;
+var container = document.createElement('div');
+elemFullScreen[0].addEventListener("click", function(){
+	if (document.querySelector('[aria-label="Проигрыватель YouTube"]')!=null){
+		if(flag>0){
+			document.body.appendChild(container);
+			container.style.position = 'absolute';
+			container.style.backgroundColor = 'rgba(14, 14, 14, 0)';
+			container.style.width = '500%';
+			container.style.height = '90%';
+			container.style.bottom = '10%';
+			container.style.left = '49%';
+			container.style.display = 'inline';
+			container.style.cursor = 'pointer';
+			flag = 0;
+		}
+		else {
+			container.style.display = 'inline';
+		}
+	}
+	else {
+		if(flag == 0){
+			container.style.display = 'none';
+			let chat = document.querySelector("#chat");
+			chat.style.display = 'none';
+		}
+	}
+});
+/*let bod = document.getElementById('content');
+bod.insertAdjacentHTML('beforeend', `
+	<div id="chat">
+		<div class="container">
+			<div class="flex-wrapper">
+			</div>
 		</div>
-		<img src = "https://pp.userapi.com/c846323/v846323645/1c625b/o_Zd0-zz3u8.jpg" style = "position: absolute;
-			widht: 24px;
-			height: 24px;
-			left: 89.63%;
-			right: 38%;
-			top: 15.34%;
-			bottom: 50.16%;
-			mix-blend-mode: multiply;
-			background-color: rgba(255, 255, 255, 0.78);"> 
+		<div class="field-container for-input">
+			<div class="circle"></div>
+			<input type="text" class="message" placeholder="Введите сообщение" id="message-field" name="message">
+		</div>
 	</div>
-    `);
+`);
+let chat = document.querySelector("#chat");
+chat.style.display = 'none';*/
+var bod = document.getElementsByTagName('body')[0];
+bod.insertAdjacentHTML('beforeend', `
+<div id="chat">
+  <div class="container">
+    <div class="flex-wrapper">
+    </div>
+  </div>
+  <div class="field-container for-input">
+    <div class="circle"></div>
+    <input type="text" class="message" placeholder="Введите сообщение" id="message-field" name="message">
+  </div>
+</div>
+`);
+let chat = document.querySelector("#chat");
+chat.style.display = 'none';
+function addMessage(message) {
+    let chat = document.querySelector("#chat .flex-wrapper");
+    let circle = document.createElement("div");
+    circle.classList.add("circle");
+    let message_div = document.createElement("div");
+    message_div.classList.add("message");
+    message_div.textContent = message;
+    let div_container = document.createElement("div");
+    div_container.classList.add("field-container");
+    div_container.appendChild(circle);
+    div_container.appendChild(message_div);
+    chat.appendChild(div_container);
+    let chat_container = document.querySelector("#chat > .container");
+    chat_container.scrollTop = chat_container.scrollHeight;
+}
+
+let input = document.getElementById("message-field");
+
+input.onkeypress = function (event) {
+    if(event.which === 13) {
+        let message = this.value;
+        addMessage(message);
+        this.value = "";
+        this.blur();
+    } else {
+        if(this.value.length >= 70) return false;
+    }
+};
+container.onmouseover = function(){
+	let chat = document.querySelector("#chat");
+	chat.style.display = 'none';
+};
+container.onmouseout = function(){
+	let chat = document.querySelector("#chat");
+	chat.style.display = 'inline';
+	let chat_container = document.querySelector("#chat .field-container");
+	chat_container.style.bottom = '0%';
+};
