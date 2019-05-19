e4c4669d04ffe42648cf14976eb62161ba3ec2bd
Victoria <31935971+vikstr@users.noreply.github.com>
2019-05-19 14:00:59 +0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.onload = function() {
-	document.getElementById("field-file").onclick = function() {
+	/*document.getElementById("field-file").onclick = function() {
 		var d = document.getElementById("login-field").value;
 		var img_ = document.getElementById("password-field").value;
 		chrome.storage.sync.set({ "name" : d }, function() {
@@ -14,7 +14,7 @@ window.onload = function() {
 				console.log("Runtime error.");
 			}
 		});
-	}
+	}*/
   document.querySelector("#log_in").addEventListener('click', function() {
     chrome.identity.getAuthToken({interactive: true}, function(token) {
       var init = {
@@ -31,10 +31,13 @@ window.onload = function() {
           init)
           .then((response) => response.json())
           .then(function(data) {
-			  let photoDiv = document.querySelector('#friendDiv');
-			  let profileImg = document.createElement('img');
-              profileImg.src = 'https://62.img.avito.st/640x480/4097554062.jpg';
-              photoDiv.appendChild(profileImg);
+			let xhr = new XMLHttpRequest();
+			xhr.open("GET", "https://reqres.in/api/products/3", true);
+			xhr.onload = function(){
+				let photoDiv = document.querySelector('#friendDiv');
+			};
+			xhr.send(null);
+			  
           });
 		chrome.storage.sync.set({"token" : token}, function() {
 			if (chrome.runtime.error) {
@@ -43,5 +46,18 @@ window.onload = function() {
 		});
 	});
       
+  });
+  document.querySelector("#submit").addEventListener('click', function() {
+	  let photoDiv = document.querySelector('#friendDiv');
+			  let profileImg = document.createElement('img');
+              profileImg.src = 'https://62.img.avito.st/640x480/4097554062.jpg';
+			  profileImg.style.position = 'relative';
+			  profileImg.style.display='inline-block';
+			  profileImg.style.width='150px';
+			  profileImg.style.height='150px';
+			  profileImg.style.borderRadius = '50%';
+			  profileImg.style.marginTop = '15px';
+			  profileImg.style.left = '40px';
+              photoDiv.appendChild(profileImg);
   });
 };
